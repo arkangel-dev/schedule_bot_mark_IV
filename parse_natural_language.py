e4493dc0eebc1be73e0060f980e4fcbc3107c5ae
athphane <athphane@gmail.com>
2019-03-21 13:43:08 +0500

    - Creating a new branch

--- parse_natural_language.py
+++ parse_natural_language.py
@@ -9,14 +9,21 @@ def getFullTodayNL():
 	todayDay = pda.convertDateToDay(datetime.today())
 	session_count = pd.getSessionCount(todayDay)
 	output_list.append("You have " + str(session_count) + " session(s) today. They are the following : ")
+	
 	for x in range(0, session_count):
 		dayData = pd.parseSessionData(x, todayDay)
 		if dayData[6]:
 			bring_laptop = True
 		output_list.append("You have " + dayData[0] + " from " + dayData[1] + " hours to " + dayData[2] + " hours with " + dayData[4] + ". Class will be held at " + str(dayData[5]) + ".")
+	
 	if bring_laptop:
 		output_list.append("You'll also need to bring your laptop. Dont forget to charge it")
 	else:
 		output_list.append("You won't be needing your laptop")
-	return(output_list)
-	
+
+	# Convert to string without messing with Isaam's code.
+	final_text = ''
+	for text in output_list:
+		final_text += text
+		
+	return(final_text)
--- test_run.py
+++ test_run.py
@@ -2,6 +2,7 @@ import parse_data as pd
 import parse_dates as pda
 import parse_natural_language as parse_nl
 from datetime import datetime
+from send_the_message import sendMessage
 
 # print(pda.convertDateToDay(datetime.today()))
 # print(pd.parseSessionData(0, "wednesday"))
