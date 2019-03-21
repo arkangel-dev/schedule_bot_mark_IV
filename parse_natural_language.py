25d50e4f886a84656956db0d64c318859e785f72
athphane <athphane@gmail.com>
2019-03-22 00:40:38 +0500

    - Initial bug fixes

--- parse_natural_language.py
+++ parse_natural_language.py
@@ -7,12 +7,17 @@ from datetime import datetime
 def getFullTodayNL():
 	output_list = []
 	bring_laptop = False
-	todayDay = pda.convertDateToDay(datetime.today())
+	todayDay = pda.convertDateToDay()
 	cancelled_session_list = p_modified_d.getCancelledSessions(todayDay)
+	cancelled_session_list_count = len(cancelled_session_list)
+
 	appended_session_list = p_modified_d.getAppendedSessions(todayDay)
+	if appended_session_list == None:
+		appended_session_list_count = 0
+
 	session_count = pd.getSessionCount(todayDay) # - len(cancelled_session_list)
 	if session_count != 0:
-		output_list.append("You have " + str((session_count - len(cancelled_session_list)) + len(appended_session_list)) + " session(s) today. They are the following : \n")
+		output_list.append("You have " + str((session_count - cancelled_session_list_count) + appended_session_list_count) + " session(s) today. They are the following : \n")
 		for x in range(0, session_count):
 			dayData = pd.parseSessionData(x, todayDay)
 			if str(x) in cancelled_session_list:
@@ -21,7 +26,7 @@ def getFullTodayNL():
 				if dayData[3]:
 					bring_laptop = True
 				output_list.append("* >  " + dayData[0] + " from " + dayData[1] + " hours to " + dayData[2] + " hours with " + dayData[4] + ". Class will be held at " + str(dayData[5]) + ".*")
-		if len(appended_session_list) != 0:
+		if appended_session_list_count != 0:
 			output_list.append("\nIn addition to your regular classes you also have the following appended class(es) : \n")
 			for x in range(0, len(appended_session_list)):
 				output_list.append("* >  " + appended_session_list[x][0] + " from " + appended_session_list[x][1] + " hours to " + appended_session_list[x][2] + " hours with " + appended_session_list[x][4] + ", at " + appended_session_list[x][5] + ".*")
@@ -34,5 +39,3 @@ def getFullTodayNL():
 	else:
 		output_list.append("You don't have any sessions today.")
 	return(output_list)
-
-	
