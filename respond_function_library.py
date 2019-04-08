c40325a40ca8e71960fddba5f7b8696863962d64
darkSrc0deR <asdurftech@gmail.com>
2019-04-08 20:57:36 +0500

    Added Status File Controller

--- respond_function_library.py
+++ respond_function_library.py
@@ -1,1 +1,22 @@
-# respond_function_library
+import core_functions as core
+
+# There function will be needed 
+# to read and write to the user status data file
+# Cool? Okay, Cool!
+
+def appendStatus_await(userid, callback_function):
+    # Ok so what we need to do is open a file,
+    # get the data from the function's parameter
+    # and then use it append an entry into the user
+    # status file dictionary and save it... 
+    # easy!
+    user_status_data = core.openJsonFile("user_status_data.json") # ok the file is open.
+    # now we append the user id as a string to the dictionary
+    user_status_data["awaiting_response_users"].update({ str(userid) : { "callback_function" : callback_function }})
+    core.saveJsonFile(user_status_data, "user_status_data.json")
+
+def deleteStatus_await(userid):
+    user_status_data = core.openJsonFile("user_status_data.json") # CopyCat ;)
+    # We got he other one working... now we fix the other
+    del user_status_data["awaiting_response_users"][str(userid)]
+    core.saveJsonFile(user_status_data, "user_status_data.json")
